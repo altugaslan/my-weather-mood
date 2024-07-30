937d21e7faafd0004cdf687a502860a277ec6217
Altug Aslan <132589981+altugaslan@users.noreply.github.com>
2024-07-30 10:50:56 +0300

    modifying the react according to the new version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import CurrentLocation from "./currentLocation";
+import "./App.css";
+import WalletButton from "./walletButton";
+import styled from 'styled-components';
+
+
+const WalletButtonContainer = styled.div`
+  position:absolute;
+  top:10px;
+  right:10px;
+  z-index:1;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <React.Fragment>
+      <div className="container">
+      <WalletButtonContainer>
+          <WalletButton/>
+        </WalletButtonContainer>
+        <CurrentLocation />
+      </div>
+      <div className="footer-info">
+        <a href="https://www.htmlhints.com/article/how-to-create-toggle-switch/93">
+          Download Source Code
+        </a>{" "}
+        | Developed by{" "}
+        <a target="_blank" href="https://www.gauravghai.dev/">
+          Gaurav Ghai
+        </a>{" "}
+        | Powered by{" "}
+        <a target="_blank" href="https://www.htmlhints.com/">
+          HTML HINTS
         </a>
-      </header>
-    </div>
+      </div>
+    </React.Fragment>
   );
 }
 
---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+export default  {
+  key: "72173cf334c53370f0683a784f33037e",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
